3409a06dafb62b0592890a24535dd0af237ad4a9
Paul Duncan <paulddncn@gmail.com>
2023-05-11 20:50:48 -0600

    updated dev

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -96,21 +96,14 @@ module.exports = () => {
         description: 'My awesome Progressive Web App!',
         background_color: '#ffffff',
         crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
-        // icons: [
-        //   {
-        //     src: path.resolve('src/assets/icon.png'),
-        //     sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
-        //   },
-        //   {
-        //     src: path.resolve('src/assets/large-icon.png'),
-        //     size: '1024x1024' // you can also use the specifications pattern
-        //   },
-        //   {
-        //     src: path.resolve('src/assets/maskable-icon.png'),
-        //     size: '1024x1024',
-        //     purpose: 'maskable'
-        //   }
-        // ]
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+            destination: path.join('assets', 'icons')
+          },
+          
+        ]
       })
     ],
 
